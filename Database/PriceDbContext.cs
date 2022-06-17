b83e9c66b175cd771a8761dc4967fbf4e4b68d49
dasdo <pamela.navia@ucb.edu.bo>
2022-06-17 01:59:22 -0400

    added context insert dependencies

---
+++ Database/PriceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Database
+{
+    public class PriceDbContext : PriceDbContext
+    {
+        private IConfiguration _configuration;
+        public DbSet<Price> Price { get; set; }
+        public PriceDbContext (IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _configuration.GetSection("Database").GetSection("ConnectionString").Value;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
