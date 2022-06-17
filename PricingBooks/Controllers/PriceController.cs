9614bef6e5fca2ed7a89a2acabd74793e86981a6
dasdo <pamela.navia@ucb.edu.bo>
2022-06-17 01:23:21 -0400

    created structure

---
+++ PricingBooks/Controllers/PriceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PricingBooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriceController : ControllerBase
+    {
+        public IActionResult GetPrices()
+        {
+            return null;
+        }
+    }
+}
+
